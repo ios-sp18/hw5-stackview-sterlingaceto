9e6e629b5dcce2cbb488d36637f2247c210ae0a8
profile <aceto013@maclab-85.uwp.edu>
2018-03-20 19:38:36 -0500

    Initial Commit

---
+++ HW5 Aceto ConstraintsTests/HW5_Aceto_ConstraintsTests.swift
@@ -0,0 +1,36 @@
+//
+//  HW5_Aceto_ConstraintsTests.swift
+//  HW5 Aceto ConstraintsTests
+//
+//  Created by Aceto, Sterling M (aceto013) on 3/20/18.
+//  Copyright © 2018 Aceto, Sterling M (aceto013). All rights reserved.
+//
+
+import XCTest
+@testable import HW5_Aceto_Constraints
+
+class HW5_Aceto_ConstraintsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
